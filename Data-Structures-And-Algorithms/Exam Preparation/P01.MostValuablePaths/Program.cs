fcb9822ee5145951e3a04bdd3152ac5e744441df
eoLFroZen <galio_96@abv.bg>
2023-10-21 10:16:58 +0300

    upload exam preparation

---
+++ Data-Structures-And-Algorithms/Exam Preparation/P01.MostValuablePaths/Program.cs
@@ -0,0 +1,37 @@
+﻿using P01.MostValuablePaths;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Tree tree = new Tree();
+        string input = null;
+
+        while ((input = Console.ReadLine()) != "calculate")
+        {
+            int[] inputNums = input.Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int parentNum = inputNums[0];
+            int childNum = inputNums[1];
+
+            tree.Add(parentNum, childNum);
+        }
+
+        List<List<int>> result = tree.CalculateTopThree()
+            .OrderByDescending(path => path.Sum())
+            .Take(3)
+            .ToList();
+
+        foreach (List<int> path in result)
+        {
+            int sum = path.Sum();
+
+            string resultLine = $"{sum} -> {string.Join(", ", path)}";
+
+            Console.WriteLine(resultLine);
+        }
+
+    }
+}
---
+++ Data-Structures-And-Algorithms/Exam Preparation/P02.HalfPalindrome/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(); ;
+        List<string> result = new List<string>();
+
+        Permute(0, input, new string[input.Length], new bool[input.Length], new HashSet<string>(), result);
+
+        Console.WriteLine(string.Join(Environment.NewLine, result.OrderBy(x =>x)));
+    }
+
+    private static void Permute(int index, string[] element, string[] subset, bool[] isUsed, HashSet<string> palindromes, List<string> result)
+    {
+        if (index == element.Length)
+        {
+            string normalStr = string.Join(" ", subset);
+            string reversedStr = string.Join(" ", subset.Reverse());
+
+            if (!palindromes.Contains(normalStr))
+            {
+                palindromes.Add(reversedStr);
+                result.Add(normalStr);
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < element.Length; i++)
+        {
+            if (!isUsed[i])
+            {
+                isUsed[i] = true;
+                subset[index] = element[i];
+                Permute(index + 1, element, subset, isUsed, palindromes, result);
+                isUsed[i] = false;
+            }
+        }
+    }
+}
